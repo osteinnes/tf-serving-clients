ea6e066900f2029caec126b7e75d4b1616b50b4a
osteinnes <omsteinn@stud.ntnu.no>
2019-02-28 10:48:06 +0100

    Simplifying main code in calc output

--- calculate_accuracy.py
+++ calculate_accuracy.py
@@ -2,17 +2,14 @@ import xml.etree.ElementTree as ET
 import numpy as np
 from scipy import stats
 
-class Accuracy_Calc:
 
+class Accuracy_Calc:
 
     def __init__(self, xml_path, output_filename):
 
         # Retrieving data from IOU xml.
         list_with_all_label_cracks, list_with_all_pred_cracks, list_with_iou, list_with_certainty, list_with_pred_class, list_with_actu_class = self.read_iou_xml(
             xml_path)
-
-        # Create file for output.
-        output_file = open(output_filename, "w+")
 
         # Do some necessary calculations for data representation
         total_pred_cracks = sum(list_with_all_pred_cracks)
@@ -27,25 +24,8 @@ class Accuracy_Calc:
         nobs, minmax, mean, variance, skewness, kurtosis = describe
 
         # Write output
-        output_file.write("ACCURACY OF OBJECT DETECTION MODEL \n")
-        output_file.write("----------------------------------------------------------\n")
-        output_file.write("Path of XML: " + xml_path + "\n")
-        output_file.write("----------------------------------------------------------\n")
-        output_file.write("Total number of predicted crakcs: " + str(total_pred_cracks) + "\n")
-        output_file.write("Total number of labeled cracks: " + str(total_actu_cracks) + "\n")
-        output_file.write("Total number of missed cracks: " + str(total_missed_cracks) + "\n")
-        output_file.write("----------------------------------------------------------\n")
-
-        output_file.write("See scipy.describe for documentation\n")
-        output_file.write("----------------------------------------------------------\n")
-        output_file.write("Nobs: " + str(nobs) + "\n")
-        output_file.write("MinMax: " + str(minmax) + "\n")
-        output_file.write("Mean: " + str(mean) + "\n")
-        output_file.write("Variance: " + str(variance) + "\n")
-        output_file.write("Skewness: " + str(skewness) + "\n")
-        output_file.write("Kurtosis: " + str(kurtosis) + "\n")
-        
-
+        self.write_output_file(output_filename, xml_path, total_pred_cracks, total_actu_cracks,
+                          total_missed_cracks, nobs, minmax, mean, variance, skewness, kurtosis)
 
     def read_iou_xml(self, xml_file: str):
 
@@ -76,8 +56,10 @@ class Accuracy_Calc:
             num_pred_cracks = None
 
             # Class name of the label in labels.
-            num_label_cracks = int(images.find("data").find("num_label_cracks").text)
-            num_pred_cracks = int(images.find("data").find("num_pred_cracks").text)
+            num_label_cracks = int(images.find(
+                "data").find("num_label_cracks").text)
+            num_pred_cracks = int(images.find(
+                "data").find("num_pred_cracks").text)
 
             # List of label names.
             list_with_all_label_cracks.append(num_label_cracks)
@@ -97,6 +79,37 @@ class Accuracy_Calc:
 
         return list_with_all_label_cracks, list_with_all_pred_cracks, list_with_iou, list_with_certainty, list_with_pred_class, list_with_actu_class
 
+        def write_output_file(self, output_filename, xml_path, total_pred_cracks, total_actu_cracks, total_missed_cracks, nobs, minmax, mean, variance, skewness, kurtosis):
+            
+            # Create file for output.
+            output_file = open(output_filename, "w+")
+            
+            output_file.write("ACCURACY OF OBJECT DETECTION MODEL \n")
+            output_file.write(
+                "----------------------------------------------------------\n")
+            output_file.write("Path of XML: " + xml_path + "\n")
+            output_file.write(
+                "----------------------------------------------------------\n")
+            output_file.write(
+                "Total number of predicted crakcs: " + str(total_pred_cracks) + "\n")
+            output_file.write(
+                "Total number of labeled cracks: " + str(total_actu_cracks) + "\n")
+            output_file.write("Total number of missed cracks: " +
+                              str(total_missed_cracks) + "\n")
+            output_file.write(
+                "----------------------------------------------------------\n")
+
+            output_file.write("See scipy.describe for documentation\n")
+            output_file.write(
+                "----------------------------------------------------------\n")
+            output_file.write("Nobs: " + str(nobs) + "\n")
+            output_file.write("MinMax: " + str(minmax) + "\n")
+            output_file.write("Mean: " + str(mean) + "\n")
+            output_file.write("Variance: " + str(variance) + "\n")
+            output_file.write("Skewness: " + str(skewness) + "\n")
+            output_file.write("Kurtosis: " + str(kurtosis) + "\n")
+
 
 if __name__ == '__main__':
-    Accuracy_Calc("/home/osteinnes/prog/tfserving-client/output/example_output.xml", "output/train_output_acc.txt")
+    Accuracy_Calc("/home/osteinnes/prog/tfserving-client/output/example_output.xml",
+                  "output/train_output_acc.txt")
